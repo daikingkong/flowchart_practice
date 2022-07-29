5f038bf49609b7062188b7ae5352e31033fa8404
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-07-29 08:10:27 +0000

    [Finish] air_flow

--- air_conditioner.rb
+++ air_conditioner.rb
@@ -2,6 +2,7 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
+air_flow = input_lines[2].to_i
 
 temperature_diff = (room - air_conditioner).abs
 
@@ -14,5 +15,30 @@ elsif temperature_diff >= 5 && temperature_diff < 10 then
 elsif temperature_diff >= 10 then
   require_time = 60
 end
+
+if air_flow == 1
+  require_time
+elsif air_flow == 2
+  require_time = require_time - 5
+elsif air_flow == 3
+  require_time = require_time - 10
+else
+  puts "風量の値は１～３にしてください"
+  exit
+end
+
+# またはcaseを使ったやり方
+# case air_flow
+# when 1 then
+#   require_time
+# when 2 then
+#   require_time = require_time - 5
+# when 3 then
+#   require_time = require_time - 10
+# else
+#   p "風量の値は1~3にしてください"
+#   exit
+# end
+
 
 p require_time
